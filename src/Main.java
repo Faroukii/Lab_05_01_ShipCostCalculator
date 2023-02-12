de22b07e57bcac187207910397a3a58aba3df812
Faroukii <122316189+Faroukii@users.noreply.github.com>
2023-02-12 17:46:42 -0500

    calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Prompt the user to enter the price of the package
+        System.out.print("Enter the price of the package: ");
+        int itemPrice = 99;
+        double shippingFee = .02;
+        double shippingCost = itemPrice * shippingFee;
+        double actualShippingCost = shippingCost + itemPrice;
+        //Calculate cost of shipping
+
+        if (itemPrice >= 100) {
+
+            System.out.println("The shipping cost is free. ");
+        } else {
+            if (itemPrice < 100);
+
+            System.out.println("Your total cost is: " + actualShippingCost);
+        }
     }
 }
+
+
